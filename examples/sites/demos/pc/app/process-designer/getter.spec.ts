7b2301fe86bd4fb199c71ecc48bef0e7824378a0
GaoNeng-wWw <gaonenggg@outlook.com>
2024-09-17 15:33:08 +0800

    test(process-designer): e2e test

--- examples/sites/demos/pc/app/process-designer/getter.spec.ts
+++ examples/sites/demos/pc/app/process-designer/getter.spec.ts
@@ -1,6 +1,6 @@
-import { expect, test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test('ProcessDesigner 静态数据', async ({ page }) => {
+test('ProcessDesigner getter', async ({ page }) => {
   page.on('pageerror', (exception) => expect(exception).toBeNull())
   await page.goto('process-designer#basic-usage')
   await expect(page.locator('g:nth-child(3) > .djs-element > .djs-hit').first()).toBeVisible()
