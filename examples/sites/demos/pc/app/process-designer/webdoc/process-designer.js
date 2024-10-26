1dc6d84742294aceea73fc6392124518e0a9f190
GaoNeng-wWw <gaonenggg@outlook.com>
2024-10-26 09:51:26 +0800

    docs(process-designer): format doc

--- examples/sites/demos/pc/app/process-designer/webdoc/process-designer.js
+++ examples/sites/demos/pc/app/process-designer/webdoc/process-designer.js
@@ -9,7 +9,7 @@ export default {
         'en-us': 'Basic Usage'
       },
       desc: {
-        'zh-CN': 'process designer的基本使用方法',
+        'zh-CN': 'process designer 的基本使用方法',
         'en-US': 'Basic usage of process designer'
       },
       codeFiles: ['index.vue']
@@ -21,8 +21,8 @@ export default {
         'en-US': 'Getter'
       },
       desc: {
-        'zh-CN': '通过传入getData函数, 实现process-designer的远程获取功能',
-        'en-US': 'Implement remote retrieval function of process designer by passing in the getData function'
+        'zh-CN': '通过传入 getData 函数, 实现 process-designer 的远程获取功能.',
+        'en-US': 'Implement remote retrieval function of process designer by passing in the getData function.'
       },
       codeFiles: ['getter.vue']
     },
@@ -34,7 +34,7 @@ export default {
       },
       desc: {
         'zh-CN':
-          '通过传入Modeler类, 从而实现自定义效果. Modeler示例请访问 <a href="https://gitlink.org.cn/opentiny/tiny-vue/tree/2c0b086607/examples/sites/demos/pc/app/process-designer/modeler/custom">custom-modeler</a>',
+          '通过传入 Modeler 类, 从而实现自定义效果. Modeler示例请访问 <a href="https://gitlink.org.cn/opentiny/tiny-vue/tree/2c0b086607/examples/sites/demos/pc/app/process-designer/modeler/custom">custom-modeler</a>',
         'en-US': 'By passing in the Modeler class, custom effects can be achieved'
       },
       codeFiles: ['custom-modeler.vue']
@@ -46,7 +46,7 @@ export default {
         'en-US': 'additional modules'
       },
       desc: {
-        'zh-CN': '通过设置<code>additional-modules</code>属性来配置追加模块, 本示例追加了小地图模块',
+        'zh-CN': '通过设置 <code>additional-modules</code> 属性来配置追加模块, 本示例追加了小地图模块。',
         'en-US':
           'By setting the<code>additive modules</code>attribute to configure additional modules, this example adds a small map module'
       },
@@ -60,7 +60,7 @@ export default {
       },
       desc: {
         'zh-CN':
-          '通过设置<code>import-xml-success</code>和<code>import-xml-error</code>来监听xml导入是否成功。如果失败则会触发<code>import-xml-error</code>',
+          '通过设置 <code>import-xml-success</code> 和 <code>import-xml-error</code> 来监听 xml 导入是否成功。如果失败则会触发 <code>import-xml-error</code>。',
         'en-US':
           'Monitor whether the XML import is successful by setting<code>import xml success</code>and<code>import xml error</code>. If it fails, it will trigger<code>import xml error</code>'
       },
@@ -73,7 +73,7 @@ export default {
         'en-US': 'properties panel'
       },
       desc: {
-        'zh-CN': '通过设置<code>show-properties</code>来控制侧边栏是否展示',
+        'zh-CN': '通过设置 <code>show-properties</code> 来控制侧边栏是否展示。',
         'en-US': 'Control whether the sidebar is displayed by setting<code>show properties</code>'
       },
       codeFiles: ['show-properties.vue']
@@ -86,9 +86,9 @@ export default {
       },
       desc: {
         'zh-CN':
-          '<code>@opentiny/tiny-bpmn</code>提供了<code>languagePackge</code>包, 汉化了<code>bpmn</code>与<code>propertiesPanel</code>。',
+          '<code>@opentiny/tiny-bpmn</code> 提供了 <code>languagePackages</code> 包, 汉化了 <code>bpmn</code> 与 <code>propertiesPanel</code>',
         'en-US':
-          '<code>@opentiny/tiny-bpmn</code> provide <code>languagePackge</code>, Sinicized<code>bpmn</code> and <code>propertiesPanel</code>'
+          '<code>@opentiny/tiny-bpmn</code> provide <code>languagePackages</code>, Sinicized<code>bpmn</code> and <code>propertiesPanel</code>'
       },
       codeFiles: ['i18n.vue']
     },
@@ -99,7 +99,7 @@ export default {
         'en-US': 'export xml'
       },
       desc: {
-        'zh-CN': '配合<code>mounted</code>事件获取<code>modeler</code>来获取xml',
+        'zh-CN': '配合 <code>mounted</code> 事件获取 <code>modeler</code> 来获取 xml',
         'en-US': 'Cooperate with<code>mounted</code>event to obtain<code>modeler</code>to obtain XML'
       },
       codeFiles: ['export-xml.vue']
@@ -111,7 +111,7 @@ export default {
         'en-US': 'readonly'
       },
       desc: {
-        'zh-CN': '使用<code>readonly</code>选项来配置该编辑器是否只读',
+        'zh-CN': '使用 <code>readonly</code> 选项来配置该编辑器是否只读。',
         'en-US': 'Use the<code>read-only</code>option to configure whether the editor is read-only'
       },
       codeFiles: ['readonly.vue']
@@ -123,7 +123,7 @@ export default {
         'en-US': 'keyboard'
       },
       desc: {
-        'zh-CN': `您可以使用<code>keyboard</code>属性来显式的指定快捷键是否开启. <br />
+        'zh-CN': `您可以使用 <code>keyboard</code> 属性来显式的指定快捷键是否开启. <br />
         默认快捷键为: 
         <ul>
           <li> <code>ctrl + z</code> 撤销</li>
