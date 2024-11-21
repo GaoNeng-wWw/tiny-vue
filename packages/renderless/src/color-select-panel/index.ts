8c225dfa199aa678efae4253361cccea00f5e2c4
GaoNeng-wWw <gaonenggg@outlook.com>
2024-11-21 15:16:45 +0800

    fix(color-select-panel): undefined length if predefine color is empty

--- packages/renderless/src/color-select-panel/index.ts
+++ packages/renderless/src/color-select-panel/index.ts
@@ -159,8 +159,8 @@ export const initState = (props: IColorSelectPanelProps, { reactive, ref, comput
     alpha,
     stack,
     predefineStack,
-    enablePredefineColor: computed(() => predefineStack.value.length),
-    enableHistory: computed(() => stack.value.length)
+    enablePredefineColor: computed(() => predefineStack.value?.length),
+    enableHistory: computed(() => stack.value?.length)
   })
   return state
 }
